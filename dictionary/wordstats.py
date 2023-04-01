60b5014525b2f1d6c4f3a107bfe97628803f9521
crutch12 <criitch@yandex.ru>
2023-04-01 16:22:59 +0300

    google daily

--- dictionary/wordstats.py
+++ dictionary/wordstats.py
@@ -5,3 +5,8 @@ def google_wordstats_json(search):
     json_str = os.popen('npx tsx ../server/bin/google.ts %s' % search).read()
 
     return json_str
+
+def google_wordstats_json_daily(search):
+    json_str = os.popen('npx tsx ../server/bin/google-daily.ts %s' % search).read()
+
+    return json_str
---
+++ server/bin/google-daily.ts
@@ -0,0 +1,9 @@
+import { googleWordStatsDaily } from "../wordstats";
+
+const [_, __, ...searchArgs] = process.argv;
+
+const search = searchArgs.join(" ");
+
+googleWordStatsDaily(search)
+  .then((data) => console.log(JSON.stringify(data)))
+  .then(() => process.exit());
